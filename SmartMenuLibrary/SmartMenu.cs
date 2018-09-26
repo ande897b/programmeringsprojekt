d94fb2598ecaaad567c909da718ddeb20d346c09
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2018-09-26 10:16:03 +0200

    if statements

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FunctionLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,24 +11,53 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
-            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\emil0\source\repos\ProjektProgrammering1(Anders,Kasper,Emil)\programmeringsprojekt\MenuSpecEN.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Anders\Desktop\gittest\pc1\SmartMenuApp.Code-master\MenuSpecEN.txt");
         }
         public void Activate()
         {
             int counter = 0;
             string line;
             char input;
-            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\emil0\source\repos\ProjektProgrammering1(Anders,Kasper,Emil)\programmeringsprojekt\MenuSpecEN.txt");
+            bool restart = true;
+            // Functions functions = new Functions();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Anders\Desktop\gittest\pc1\SmartMenuApp.Code-master\MenuSpecEN.txt");
             while ((line = file.ReadLine()) != null)
             {
                 Console.WriteLine(line);
                 counter++;
             }
+            while (restart)
+            {
+                input = char.Parse(Console.ReadLine());
 
-            input=char.Parse(Console.ReadLine());
+                if (input == '1')
+                {
+                    Console.WriteLine(Functions.DoThis());
+                    restart = false;
+                }
 
-
-
+               else if (input == '2')
+                {
+                    Console.WriteLine(Functions.DoThat());
+                    restart = false;
+                }
+               else if (input == '3')
+                {
+                    string inputTilDoSomething = Console.ReadLine();
+                    // Functions.DoSomething(inputTilDoSomething);
+                    Console.WriteLine(Functions.DoSomething(inputTilDoSomething));
+                    restart = false;
+                }
+              else  if (input == '4')
+                {
+                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                    restart = false;
+                }               
+                else 
+                {
+                    Console.WriteLine("Input proper value");                    
+                }
+            }
         }
     }
 }
