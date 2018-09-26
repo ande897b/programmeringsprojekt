1130d32bb2fa2cd5d1136d844c495b6df8ebe034
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2018-09-26 11:20:42 +0200

    valg med menu, mangler dansk menu

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -15,48 +15,74 @@ namespace SmartMenuLibrary
         }
         public void Activate()
         {
-            int counter = 0;
-            string line;
-            char input;
-            bool restart = true;
-            // Functions functions = new Functions();
-            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Anders\Desktop\gittest\pc1\SmartMenuApp.Code-master\MenuSpecEN.txt");
-            while ((line = file.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-                counter++;
-            }
-            while (restart)
-            {
-                input = char.Parse(Console.ReadLine());
 
-                if (input == '1')
+            Console.WriteLine("For english press 1 followed by enter, for danish press 2 followed by enter");
+            char valgIMenu;
+            bool menuBool = true;
+        
+            while (menuBool)
+            {
+               valgIMenu= char.Parse(Console.ReadLine());
+
+                if (valgIMenu == '1')
                 {
-                    Console.WriteLine(Functions.DoThis());
-                    restart = false;
+
+                    int counter = 0;
+                    string line;
+                    char input;
+                    bool restart = true;
+                    // Functions functions = new Functions();
+                    System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Anders\Desktop\gittest\pc1\SmartMenuApp.Code-master\MenuSpecEN.txt");
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        counter++;
+                    }
+                    while (restart)
+                    {
+                        input = char.Parse(Console.ReadLine());
+
+                        if (input == '1')
+                        {
+                            Console.WriteLine(Functions.DoThis());
+                            restart = false;
+                        }
+
+                        else if (input == '2')
+                        {
+                            Console.WriteLine(Functions.DoThat());
+                            restart = false;
+                        }
+                        else if (input == '3')
+                        {
+                            string inputTilDoSomething = Console.ReadLine();
+                            // Functions.DoSomething(inputTilDoSomething);
+                            Console.WriteLine(Functions.DoSomething(inputTilDoSomething));
+                            restart = false;
+                        }
+                        else if (input == '4')
+                        {
+                            Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                            restart = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Input proper value");
+                        }
+                    }
+                    menuBool = false;
+                }
+                else if (valgIMenu == '2')
+                {
+                    Console.WriteLine("123");
+                    menuBool = false;
                 }
 
-               else if (input == '2')
+                else
                 {
-                    Console.WriteLine(Functions.DoThat());
-                    restart = false;
+                    Console.WriteLine("Input Proper Value");
                 }
-               else if (input == '3')
-                {
-                    string inputTilDoSomething = Console.ReadLine();
-                    // Functions.DoSomething(inputTilDoSomething);
-                    Console.WriteLine(Functions.DoSomething(inputTilDoSomething));
-                    restart = false;
-                }
-              else  if (input == '4')
-                {
-                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
-                    restart = false;
-                }               
-                else 
-                {
-                    Console.WriteLine("Input proper value");                    
-                }
+
             }
         }
     }
